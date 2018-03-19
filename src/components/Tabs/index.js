a93d50510f4a354070e7082394be71d5fdee6bbf
juntaozhou05 <350529307@qq.com>
2018-03-19 15:04:23 +0800

    add datas

--- src/components/Tabs/index.js
+++ src/components/Tabs/index.js
@@ -2,43 +2,84 @@ import React, { Component } from "react";
 
 import { Tabs, WhiteSpace } from "antd-mobile";
 
-class Demo extends React.Component {
-  renderContent = tab => (
-    <div
-      style={{
-        display: "flex",
-        height: "700px",
-        backgroundColor: "#fff",
-        wordBreak: "break-all",
-        padding: "10px"
-      }}
-    >
-      <p>
-        Content 111111111111111111111111111111111111111111111111111of{" "}
-        {tab.title}
-      </p>
-    </div>
-  );
+import "./tabs.css";
+
+class Demo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      classid: 0,
+      page: 1,
+      lists: []
+    };
+  }
+
+  componentDidMount = () => {
+    this.getData(this.state.classid, this.state.page);
+  };
+
+  getData(classid, page) {
+    fetch("http://data.toutiaojk.com/extend/list/appclass.php", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+      },
+      body: `classid=${classid}&page=${page}`
+    })
+      .then(res => res.json())
+      .then(res =>
+        this.setState({
+          lists: res
+        })
+      )
+      .catch(error => console.log(error));
+  }
+
   change(key) {
     console.log(key);
+    this.getData(key.classid, 1);
   }
+
+  renderContent = tab => (
+    <div className="tabs">
+      <div className="content">
+        {this.state.lists.map((item, index) => {
+          return (
+            <div className="items" key={index}>
+              <div className="titles">{item.title}</div>
+              <div className="imgs">
+                <img src={item.titlepic} alt="" />
+                <img src={item.titlepic2} alt="" />
+                <img src={item.titlepic3} alt="" />
+              </div>
+              <div className="bottom">
+                {item.befrom} {item.onclick}阅读
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+
   render() {
     const tabs = [
-      { title: "头条", class: 0 },
-      { title: "女性", class: 1 },
-      { title: "育儿", class: 2 },
-      { title: "中医", class: 3 },
-      { title: "本地", class: 4 },
-      { title: "政策", class: 5 },
-      { title: "产业", class: 6 },
-      { title: "旅游", class: 7 }
+      { title: "头条", classid: 0 },
+      { title: "女性", classid: 1 },
+      { title: "育儿", classid: 2 },
+      { title: "中医", classid: 3 },
+      { title: "本地", classid: 4 },
+      { title: "政策", classid: 5 },
+      { title: "产业", classid: 6 },
+      { title: "旅游", classid: 7 }
     ];
 
     return (
       <div>
         <WhiteSpace />
         <Tabs
-          onTabClick={key => {
+          distanceToChangeTab={0.8}
+          onChange={key => {
             this.change(key);
           }}
           tabs={tabs}
